4d1cea400a0022f536e5f83d9a03148f97560171
Emily Lemos <157964741+mlylemos@users.noreply.github.com>
2026-03-06 01:54:04 -0300

    Improve error handling in obterTaxa method
    Handle API error response and improve error logging.

--- src/conversordemoedas/ApiExchange.java
+++ src/conversordemoedas/ApiExchange.java
@@ -1,26 +1,21 @@
 package conversordemoedas;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class ApiExchange {
-
     private static final String API_KEY = "8d0902c1b2b924b3e4319a7a";
 
     public double obterTaxa(String base, String destino) {
-
         String url = "https://v6.exchangerate-api.com/v6/" +
                 API_KEY + "/pair/" + base + "/" + destino;
-
         try {
-
             HttpClient client = HttpClient.newHttpClient();
-
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
                     .GET()
@@ -32,10 +27,16 @@ public class ApiExchange {
             JsonElement elemento = JsonParser.parseString(response.body());
             JsonObject json = elemento.getAsJsonObject();
 
+            String resultado = json.get("result").getAsString();
+            if (!resultado.equals("success")) {
+                System.out.println("Erro na API: " + resultado);
+                return 0;
+            }
+
             return json.get("conversion_rate").getAsDouble();
 
         } catch (Exception e) {
-            System.out.println("Erro ao acessar API.");
+            System.out.println("Erro ao acessar a API: " + e.getMessage());
             return 0;
         }
     }
