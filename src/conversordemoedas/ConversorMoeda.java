88020d05be9d712a17c871b0ef44ff03c5f4beb8
Emily Lemos <157964741+mlylemos@users.noreply.github.com>
2026-03-06 01:54:27 -0300

    Refactor converter method in ConversorMoeda class

--- src/conversordemoedas/ConversorMoeda.java
+++ src/conversordemoedas/ConversorMoeda.java
@@ -1,13 +1,10 @@
 package conversordemoedas;
 
 public class ConversorMoeda {
-
     ApiExchange api = new ApiExchange();
 
     public double converter(String moeda1, String moeda2, double valor) {
-
         double taxa = api.obterTaxa(moeda1, moeda2);
-
         return valor * taxa;
     }
 }
